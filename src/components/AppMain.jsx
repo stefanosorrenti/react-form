e5b74bdd069e51025b92d9e03e018e8ddb965c60
stefanosorrenti <stefanosorrenti01@gmail.com>
2026-03-12 15:20:43 +0100

    AppMain: add button for remove element

--- src/components/AppMain.jsx
+++ src/components/AppMain.jsx
@@ -8,17 +8,30 @@ export default function AppMain() {
     //let articlesList = ['Pane', 'Uova', 'Mozzarella', 'Zucchine', 'Pasta',] //Array di partenza
     const [userValue, setUserValue] = useState('')
     //FUNCTIONS
-    function getDynamicSubmit (e) {  //Funzione che esegue della logica al quando viene esguito il submit in un form
+    function getDynamicSubmit(e) {  //Funzione che esegue della logica al quando viene esguito il submit in un form
         e.preventDefault() //Blocco il comportamento di default del form
         //console.log('Evento non inviato');
         console.log(`Il valore scritto dall'utente è ${userValue}`);
         const newList = [userValue, ...shopList]
         console.log(`Il nuovo array è ${newList}`);
         setShopList(newList)
-       
+        setUserValue('')
     }
 
-    
+    function deleteElement(elementIndex) {
+        const updatedList = shopList.filter((itemList, index) => {
+            if(index !== elementIndex) {
+                return true
+
+            }
+            return false
+        })
+
+        setShopList(updatedList)
+        
+    }
+
+
     return (
 
         /* Main */
@@ -37,7 +50,9 @@ export default function AppMain() {
             <div>
                 <ul>
                     {shopList.map((article, index) => ( //Aggiungo un map per avere un array della stessa lunghezza ma legegrmente modificato
-                        <li key={index}>{article}</li> //Ad ogni iterazione verra creato un tag li con il singolo elemento dell'array.
+                        <li key={index}>{article} {/* Ad ogni iterazione verra creato un tag li con il singolo elemento dell'array. */}
+                            <button onClick={() => deleteElement(index)}>Elimina</button>
+                        </li> 
                     ))}
                 </ul>
             </div>
