1e07b24ee8840a83e2913498b283f5644a6f17dd
stefanosorrenti <stefanosorrenti01@gmail.com>
2026-03-12 14:33:50 +0100

    AppMain: complete the logic for the input (Milestone 2)

--- src/components/AppMain.jsx
+++ src/components/AppMain.jsx
@@ -4,14 +4,21 @@ import { useState } from "react"
 
 export default function AppMain() {
     //DATA
-    const articlesList = ['Pane', 'Uova', 'Mozzarella', 'Zucchine', 'Pasta',]
-    
+    const [shopList, setShopList] = useState(['Pane', 'Uova', 'Mozzarella', 'Zucchine', 'Pasta'])
+    //let articlesList = ['Pane', 'Uova', 'Mozzarella', 'Zucchine', 'Pasta',] //Array di partenza
+    const [userValue, setUserValue] = useState('')
     //FUNCTIONS
     function getDynamicSubmit (e) {  //Funzione che esegue della logica al quando viene esguito il submit in un form
         e.preventDefault() //Blocco il comportamento di default del form
-        console.log('Evento non inviato');
-        
+        //console.log('Evento non inviato');
+        console.log(`Il valore scritto dall'utente è ${userValue}`);
+        const newList = [userValue, ...shopList]
+        console.log(`Il nuovo array è ${newList}`);
+        setShopList(newList)
+       
     }
+
+    
     return (
 
         /* Main */
@@ -20,8 +27,8 @@ export default function AppMain() {
             {/* FORM SECTION */}
             <section>
                 <form onSubmit={getDynamicSubmit}> {/* Aggiungo un evento per ascotlare al submit del form e passo uan funzione che esguira della logica. */}
-                    <input type="text" />
-                    <button>Aggiungi articolo</button>
+                    <input type="text" onChange={e => setUserValue(e.target.value)} value={userValue} />
+                    <button type="submit">Aggiungi articolo</button>
                 </form>
             </section>
 
@@ -29,7 +36,7 @@ export default function AppMain() {
             {/* Shopping list */}
             <div>
                 <ul>
-                    {articlesList.map((article, index) => ( //Aggiungo un map per avere un array della stessa lunghezza ma legegrmente modificato
+                    {shopList.map((article, index) => ( //Aggiungo un map per avere un array della stessa lunghezza ma legegrmente modificato
                         <li key={index}>{article}</li> //Ad ogni iterazione verra creato un tag li con il singolo elemento dell'array.
                     ))}
                 </ul>
